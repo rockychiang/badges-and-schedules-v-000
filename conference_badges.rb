0def37e44dc82a34d6901d77f77cf3ee7b9c0285
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-09 22:16:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,8 +8,8 @@ def batch_badge_creator(names)
   return badge_array
 end
 
-def assign_rooms(names)
+def assign_rooms(name)
   room = []
-  names.each{|i| room << "Hello, #{i}! You'll be assigned to room #{i.index}!"}
+  name.each{|i| room << "Hello, #{i}! You'll be assigned to room #{i.index}!"}
   return room
 end
